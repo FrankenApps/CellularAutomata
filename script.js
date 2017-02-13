976ed6c212687b8f7534d87aecfd955cef4747cb
FrankenApps <de.frankenapps@gmail.com>
2017-02-13 13:53:53 +0100

    Added rule change menu

--- script.js
+++ script.js
@@ -24,37 +24,9 @@ $( document ).ready(function() {
             }))
         .append("g");
 
-  var controls = d3.select("#controlBar")
-            .append("svg")
-            .attr("width", '100%')
-            .attr("height", '100%');
-
-  for (var i = 0; i < 32; i++) {
-    if (i%4==0) {
-      controls.append("rect")
-            .attr("x", i*30+60)
-            .attr("y", 40)
-            .attr("width", 29.5)
-            .attr("height", 29.5)
-            .style("fill", "none")
-            .style("stroke-width", "0.6")
-            .style("stroke", "#999999")
-            .attr('id', "_"+String(i));
-    }  else {
-      controls.append("rect")
-            .attr("x", i*30)
-            .attr("y", 10)
-            .attr("width", 29.5)
-            .attr("height", 29.5)
-            .style("fill", "none")
-            .style("stroke-width", "0.6")
-            .style("stroke", "#999999")
-            .attr('id', "_"+String(i));
-    }
-  }
-
     createGrid(svg);
     fillGrid();
+    createControlBar();
 });
 
 function createGrid(svg){
@@ -81,8 +53,10 @@ function createGrid(svg){
 }
 
 function fillGrid(){
+  var columnStartingPoint=iterations-1;
+  var columnEndPoint = iterations+2;
   for (var line = 1; line < iterations; line++) {
-    for (var i = 1; i < Math.ceil(2*iterations); i++) {
+    for (var i = columnStartingPoint; i < columnEndPoint; i++) {
       if (d3.select('#_' + String(i-1) + '_' + String(line-1)).style('fill')==rule[1]&&d3.select('#_' + String(i) + '_' + String(line-1)).style('fill')==rule[2]&&d3.select('#_' + String(i+1) + '_' + String(line-1)).style('fill')==rule[3]) {
         d3.select('#_' + String(i) + '_' + String(line)).style('fill', rule[0]);
       } else if (d3.select('#_' + String(i-1) + '_' + String(line-1)).style('fill')==rule[5]&&d3.select('#_' + String(i) + '_' + String(line-1)).style('fill')==rule[6]&&d3.select('#_' + String(i+1) + '_' + String(line-1)).style('fill')==rule[7]) {
@@ -101,5 +75,74 @@ function fillGrid(){
         d3.select('#_' + String(i) + '_' + String(line)).style('fill', rule[28]);
       }
     }
+    columnStartingPoint--;
+    columnEndPoint++;
   }
+}
+
+function fillGridRandom(){
+  for (var line = 1; line < iterations; line++) {
+  for (var i = 1; i < Math.ceil(2*iterations); i++) {
+    if (d3.select('#_' + String(i-1) + '_' + String(line-1)).style('fill')==rule[1]&&d3.select('#_' + String(i) + '_' + String(line-1)).style('fill')==rule[2]&&d3.select('#_' + String(i+1) + '_' + String(line-1)).style('fill')==rule[3]) {
+      d3.select('#_' + String(i) + '_' + String(line)).style('fill', rule[0]);
+    } else if (d3.select('#_' + String(i-1) + '_' + String(line-1)).style('fill')==rule[5]&&d3.select('#_' + String(i) + '_' + String(line-1)).style('fill')==rule[6]&&d3.select('#_' + String(i+1) + '_' + String(line-1)).style('fill')==rule[7]) {
+      d3.select('#_' + String(i) + '_' + String(line)).style('fill', rule[4]);
+    } else if (d3.select('#_' + String(i-1) + '_' + String(line-1)).style('fill')==rule[9]&&d3.select('#_' + String(i) + '_' + String(line-1)).style('fill')==rule[10]&&d3.select('#_' + String(i+1) + '_' + String(line-1)).style('fill')==rule[11]) {
+      d3.select('#_' + String(i) + '_' + String(line)).style('fill', rule[8]);
+    } else if (d3.select('#_' + String(i-1) + '_' + String(line-1)).style('fill')==rule[13]&&d3.select('#_' + String(i) + '_' + String(line-1)).style('fill')==rule[14]&&d3.select('#_' + String(i+1) + '_' + String(line-1)).style('fill')==rule[15]) {
+      d3.select('#_' + String(i) + '_' + String(line)).style('fill', rule[12]);
+    } else if (d3.select('#_' + String(i-1) + '_' + String(line-1)).style('fill')==rule[17]&&d3.select('#_' + String(i) + '_' + String(line-1)).style('fill')==rule[18]&&d3.select('#_' + String(i+1) + '_' + String(line-1)).style('fill')==rule[19]) {
+      d3.select('#_' + String(i) + '_' + String(line)).style('fill', rule[16]);
+    } else if (d3.select('#_' + String(i-1) + '_' + String(line-1)).style('fill')==rule[21]&&d3.select('#_' + String(i) + '_' + String(line-1)).style('fill')==rule[22]&&d3.select('#_' + String(i+1) + '_' + String(line-1)).style('fill')==rule[23]) {
+      d3.select('#_' + String(i) + '_' + String(line)).style('fill', rule[20]);
+    } else if (d3.select('#_' + String(i-1) + '_' + String(line-1)).style('fill')==rule[25]&&d3.select('#_' + String(i) + '_' + String(line-1)).style('fill')==rule[26]&&d3.select('#_' + String(i+1) + '_' + String(line-1)).style('fill')==rule[27]) {
+      d3.select('#_' + String(i) + '_' + String(line)).style('fill', rule[24]);
+    } else if (d3.select('#_' + String(i-1) + '_' + String(line-1)).style('fill')==rule[29]&&d3.select('#_' + String(i) + '_' + String(line-1)).style('fill')==rule[30]&&d3.select('#_' + String(i+1) + '_' + String(line-1)).style('fill')==rule[31]) {
+      d3.select('#_' + String(i) + '_' + String(line)).style('fill', rule[28]);
+    }
+  }
+}
+}
+
+function createControlBar(){
+  var controls = d3.select("#controlBar")
+            .append("svg")
+            .attr("width", '100%')
+            .attr("height", '100%');
+
+  for (var i = 0; i < 32; i++) {
+    if (i%4==0) {
+      controls.append("rect")
+            .attr("x", i*30+60)
+            .attr("y", 40)
+            .attr("width", 29.5)
+            .attr("height", 29.5)
+            .attr('class', 'controlBarButton')
+            .style("stroke-width", "0.6")
+            .style("stroke", "#999999")
+            .attr('id', "_c"+String(i));
+    }  else {
+      controls.append("rect")
+            .attr("x", i*30)
+            .attr("y", 10)
+            .attr("width", 29.5)
+            .attr("height", 29.5)
+            .attr('class', 'controlBarButton')
+            .style("stroke-width", "0.6")
+            .style("stroke", "#999999")
+            .attr('id', "_c"+String(i));
+    }
+  }
+
+  d3.selectAll(".controlBarButton")
+    .classed("white",function(d,i){return rule[i] === "none"})
+    .on("click",function(){
+       d3.select(this).classed("white",!d3.select(this).classed("white"));
+       alert(rule[String(this.id).substr(2)])
+       if(d3.select(this).classed('white')){
+         rule[String(this.id).substr(2)]='none';
+       }  else {
+         rule[String(this.id).substr(2)]='rgb(0, 0, 0)';
+       }
+    });
 }
